4cec40129de7d07beb70cb2058362a1095e882d4
Mike M <mike.d.meier@gmail.com>
2012-03-31 11:26:43 -0600

    fix Staff() picks

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -20,10 +20,11 @@
 import string
 import datetime
 
-RECENT_SHOWS = "http://www.archive.org/search.php?query=collection%3Aetree%26sort%3D-%2Fmetadata%2Fpublicdate"
+RECENT_SHOWS = "http://archive.org/search.php?query=collection%3Aetree&sort=-publicdate"
 MOST_DOWNLOADED = "http://www.archive.org/search.php?query=%28%28collection%3Aetree%20OR%20mediatype%3Aetree%29%20AND%20NOT%20collection%3AGratefulDead%29%20AND%20-mediatype%3Acollection&sort=-downloads"
 MOST_DOWNLOADED_WEEK = "http://www.archive.org/search.php?query=%28%28collection%3Aetree%20OR%20mediatype%3Aetree%29%20AND%20NOT%20collection%3AGratefulDead%29%20AND%20-mediatype%3Acollection&sort=-week"
 ARTISTS_URL = "http://www.archive.org/advancedsearch.php?q=mediatype%3Acollection+collection%3Aetree&fl[]=creator&fl[]=identifier&sort[]=identifier+asc&sort[]=&sort[]=&rows=50000&page=1&fmt=xml&xmlsearch=Search#raw"
+BASE_URL = "http://archive.org"
 
 ###################################################################################################
 def Start():
@@ -33,7 +34,10 @@ def Start():
   ObjectContainer.title1 = 'Live Music Archive'
   ObjectContainer.content = 'Items'
   ObjectContainer.art = R('art-default.jpg')
-  DirectoryObject.thumb=R('icon-default.png')
+  DirectoryObject.thumb = R('icon-default.png')
+  AlbumObject.thumb = R('icon-default.png')
+  #AlbumObject.art = R('art-default.jpg')
+  TrackObject.thumb = R('icon-default.png')
   HTTP.CacheTime = CACHE_1HOUR
 
 ###################################################################################################
@@ -43,20 +47,20 @@ def MainMenu():
   oc.add(DirectoryObject(key=Callback(Letters), title="Browse Archive by Artist"))
   '''TODO >>> write a LMA Search Service'''
   #dir.Append(Function(InputDirectoryItem(showList, title="Seach the Live Music Archive", prompt="Search..."), title2="Search Results"))
-  
-  oc.add(DirectoryObject(key=Callback(ShowList, title2="This Day in History", pageURL=Callback(TodayURL())), title="Shows This Day in History"))
+  todayURL = TodayURL()
+  oc.add(DirectoryObject(key=Callback(ShowList, title2="This Day in History", pageURL=todayURL), title="Shows This Day in History"))
   oc.add(DirectoryObject(key=Callback(ShowList, title2="Recently Added Shows", pageURL=RECENT_SHOWS), title="Most Recently Added Shows"))
   oc.add(DirectoryObject(key=Callback(ShowList, title2="Most Downloaded", pageURL=MOST_DOWNLOADED), title="Most Downloaded Shows"))
   oc.add(DirectoryObject(key=Callback(ShowList, title2="Last Week", pageURL=MOST_DOWNLOADED_WEEK), title="Most Downloaded Shows Last Week"))
   oc.add(DirectoryObject(key=Callback(Staff), title="Staff Picks"))
   
-  if itunesPage() != None:
+  if iTunesPage() != None:
     oc.add(ObjectDirectory(key=Callback(iTunes), title="Find Shows for Artists in my iTunes Library"))
     oc.add(PrefsObject("Preferences...",
            summary="No PMS instance with a valid iTunes library at this address (default: localhost)\n Please enter the IP address of a PMS instance sharing an iTunes library.",
            thumb=S('Gear.png')))
   else:
-    oc.add(PrefsObject("Preferences...", thumb=S('Gear.png')))
+    oc.add(PrefsObject(title="Preferences...", thumb=S('Gear.png')))
   return oc
 
 ##################################################################################################
@@ -72,8 +76,7 @@ def Letters():
 ##################################################################################################
 
 def Artists(letter=None):
-  oc = ObjectContatiner(title2="Artists-%s" % letter, view_group='List')
-
+  oc = ObjectContainer(title2="Artists-%s" % letter, view_group='List')
   artistsList = XML.ElementFromURL(ARTISTS_URL, errors='ignore',)
   results = artistsList.xpath("/response//doc")
   for n in range(len(results)):
@@ -95,14 +98,14 @@ def Artists(letter=None):
     else:
       if identifier[0] == letter:
         pageURL= "http://www.archive.org/search.php?query=collection%3A" + identifier + "&sort=-date&page=1"
-        oc.add(DirectoryObject(key=Callback(ShowList, pageURL=pageURL, title2=name, isArtistPage=True, identifier=identifier), title=name))
+        oc.add(DirectoryObject(key=Callback(ShowList, title2=name, pageURL=pageURL, isArtistPage=True, identifier=identifier, artist=name), title=name))
 
   return oc
 
 ##################################################################################################
 
-def ShowList(title2, pageURL=None, isArtistPage=False, identifier=None, query=None, thumbs=None):
-  oc = MediaContainer(title2=title2, view_group='List')
+def ShowList(title2, pageURL=None, isArtistPage=False, identifier=None, query=None, thumbs=None, artist=None):
+  oc = ObjectContainer(title2=title2, view_group='InfoList')
   if thumbs == None:
     thumbs = R('icon-default.png')
   if query != None:
@@ -116,6 +119,8 @@ def ShowList(title2, pageURL=None, isArtistPage=False, identifier=None, query=No
     # auto detect show numbers and split by year if high
     if isArtistPage == True:
       numShows = showsList.xpath("//div[3]//tr[2]//td[1]//b[2]//text()")
+      if numShows[0] == '0':
+        return ObjectContainer(header='Live Music Archive', message="No concerts found.")
       if numShows != []:
         numShows = int(numShows[0].replace(",",""))
         if numShows >= 51:
@@ -124,71 +129,68 @@ def ShowList(title2, pageURL=None, isArtistPage=False, identifier=None, query=No
           years = yearsPage.xpath("//table[@id='browse']//ul//a/text()")
           yearURLs = yearsPage.xpath("//table[@id='browse']//ul//a/@href")
           for year, url in zip(years, yearURLs):
-            oc.add(DirectoryObject(key=Callback(ShowList, title2=str(year), pageURL="http://www.archive.org" + url + "&sort=date"), title=str(year), thumb=thumbs))
+            oc.add(DirectoryObject(key=Callback(ShowList, title2=str(year), pageURL="http://www.archive.org" + url + "&sort=date", artist=artist), title=str(year), thumb=thumbs))
           return oc
 
 
     showURLs = showsList.xpath("//a[@class='titleLink']/@href")
     showTitles = showsList.xpath("//a[@class='titleLink']")
-    # pain in my fucking ass roundabout way to get propper show titles for artists split by date
+    # pain in my fucking ass roundabout way to get proper show titles for artists split by date
     titles = []
+    summaries = []
+    ratings = []
     for i in range(len(showTitles)):
       y = showsList.xpath("//table[@class='resultsTable']//tr[%i]/td[2]/a[1]//text()" % (i+1))
       # the +1 is because python list indexes start from 0 and indexes in xpath start at 1
       title = ''.join(y)
       titles.append(title)
-    
-    for url, title in zip(showURLs, titles):        
       
+      x = showsList.xpath("//table[@class='resultsTable']//tr[%i]//td[@class='hitCell']/text()" % (i+1))[1]
+      # the +1 is because python list indexes start from 0 and indexes in xpath start at 1
+      summary = ''.join(x)
+      summaries.append(summary)
+      
+      w = showsList.xpath("//table[@class='resultsTable']//tr[%i]//img[contains(@src, '/images/stars')]" % (i+1))
+      # the +1 is because python list indexes start from 0 and indexes in xpath start at 1
+      try:
+        rating = float(w[0].get('title').split(' ')[0])*2
+      except:
+        rating = None
+      ratings.append(rating)
+      
+    for url, title, summary, rating in zip(showURLs, titles, summaries, ratings):
       # for artists in the search results
       if showsList.xpath("//a[@class='titleLink'][@href='%s']/parent::td/preceding-sibling::td/img[@alt='[collection]']" %url):
         pageURL= "http://www.archive.org/search.php?query=collection%3A" + url.replace("/details/","") + "&sort=-date&page=1"
-        oc.add(DirectoryObject(key=Callback(ShowList, pageURL=pageURL, title2=title, isArtistPage=True, identifier=url.replace("/details/","")), title=title))
+        oc.add(DirectoryObject(key=Callback(ShowList, pageURL=pageURL, title2=title, isArtistPage=True, identifier=url.replace("/details/",""), artist=artist),
+          title=title))
 
       else:
-        ### REFERENCES TO "Concert()" SHOULD BE REPLACED WITH "AlbumObject()"s POINTING TO THE URL SERVICE TO RETRIEVE "TrackObject()"s ###
-        oc.add(DirectoryObject(key=Callback(Concert, page=str(url), showName=str(title)), title=str(title), thumb=thumbs))
+        concertURL = ConcertURL(url)
+        oc.add(AlbumObject(url=concertURL,  title=str(title), summary=summary, rating=rating, artist=artist))
 
     next = showsList.xpath("//a[text()='Next']/@href")
     if next != []:
       pageURL = "http://www.archive.org" + next[0]
-      oc.add(ObjectDirectory(key=Callback(ShowList, pageURL=pageURL, title2=title2), title="Next 50 Results"))
+      oc.add(DirectoryObject(key=Callback(ShowList, pageURL=pageURL, title2=title2, artist=artist), title="Next 50 Results"))
 
   return oc
 
 ##################################################################################################
 
-### REFERENCES TO "Concert()" SHOULD BE REPLACED WITH "AlbumObject()"s POINTING TO THE URL SERVICE TO RETRIEVE "TrackObject()"s ###
-def Concert(sender, page, showName):
-  dir = MediaContainer(title2=showName)
-  page = HTML.ElementFromURL("http://www.archive.org" + page, errors="ignore")
-  artist = str(page.xpath("//div[3]/a[3]/text()")).strip("[]'")
-  album = str(page.xpath("//span[text()='Date:']/following-sibling::*[1]/text()")).strip("[]'")
-  urls = []
-  
-  # m3u stream fallback for grateful dead soundboards
-  try:
-    m3u = page.xpath("//a[text()='VBR M3U']/@href")[0]
-    if m3u[0] is not 'h': m3u = 'http://www.archive.org/' + m3u
-    m3u = HTTP.Request(m3u).content.strip().splitlines()
-    Log('using m3u')
-    for url in m3u:
-      dir.Append(TrackItem(url, title="Track %i" % (m3u.index(url)+1), artist=artist, album=album, thumb=R('icon-default.png')))
-        
-  except: Log('nothing found')
-  
-  return dir
+def ConcertURL(url):
+  return "http://www.archive.org" + url
 
 ##################################################################################################
-# staff picks top level menu
+
 def Staff():
   oc = ObjectContainer(title2="Staff Picks")
   page = HTML.ElementFromURL("http://www.archive.org/details/etree", errors="ignore")
-  titles = page.xpath("//div[@id='picks']//a//text()")
-  urls = page.xpath("//div[@id='picks']//a//@href")
-  for url, title in zip(urls, titles):
-    ### REFERENCES TO "Concert()" SHOULD BE REPLACED WITH "AlbumObject()"s POINTING TO THE URL SERVICE TO RETRIEVE "TrackObject()"s ###
-    oc.add(ObjectDirectory(key=Callback(Concert), page=str(url), showName=str(title)), title=str(title))
+  picks = page.xpath("//div[@id='picks']//a")
+  for pick in picks:
+    title = pick.text
+    url = BASE_URL + pick.get('href')
+    oc.add(AlbumObject(url=url, title=title, thumb=R('icon-default.png')))
 
   return oc
 
